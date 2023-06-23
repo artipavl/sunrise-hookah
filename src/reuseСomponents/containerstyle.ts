32c9454515175563f14cba619c6c2e875b254d3d
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-06-23 17:35:58 +0300

    Create containerstyle.ts

---
+++ src/reuseСomponents/containerstyle.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding-left: 20px;
+  padding-right: 20px;
+  max-width: 1440px;
+
+  @media screen and (min-width: 1440px) {
+    padding: 0;
+    margin: auto;
+  }
+`;
