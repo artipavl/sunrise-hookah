2435676b2f85d8d21c77ca9f8c975638c726e7d8
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-06-23 17:35:03 +0300

    add About

---
+++ src/components/about/about.style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const AboutTitle = styled.h2`
+  text-align: center;
+  font-size: 4vw;
+  color: #fff;
+  text-transform: uppercase;
+`;
+
+export const Aboutlist = styled.ul`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 20px;
+  justify-content: center;
+`;
+
+export const AboutItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+
+  width: 280px;
+
+  @media screen and (min-width: 950px) {
+    width: calc((100% - 40px) / 3);
+  }
+`;
+
+export const AboutItemTitle = styled.h3`
+  font-size: 18px;
+  color: yellow;
+  text-transform: uppercase;
+`;
+export const AboutItemText = styled.p`
+  font-size: 16px;
+  color: #fff;
+  text-align: center;
+`;
---
+++ src/components/about/about.tsx
@@ -0,0 +1,76 @@
+import React, { FC } from 'react';
+import { Section } from '../../reuseСomponents/section.style';
+import { Container } from '../../reuseСomponents/containerstyle';
+import { ReactComponent as QUALITY } from '../../images/about/QUALITY.svg';
+import { ReactComponent as Stars } from '../../images/about/stars.svg';
+import { ReactComponent as Gear } from '../../images/about/gear.svg';
+import { ReactComponent as Moon } from '../../images/about/moon.svg';
+import { ReactComponent as Biceps } from '../../images/about/biceps.svg';
+import { ReactComponent as Coins } from '../../images/about/coins.svg';
+import {
+  AboutItem,
+  AboutItemText,
+  AboutItemTitle,
+  AboutTitle,
+  Aboutlist,
+} from './about.style';
+
+type AboutProps = {};
+
+const About: FC<AboutProps> = props => {
+  return (
+    <Section id="about" bgc="#000">
+      <Container>
+        <AboutTitle>SUNRISE HOOKAH - це</AboutTitle>
+        <Aboutlist>
+          <AboutItem>
+            <QUALITY color="#fff" width={'50%'} />
+            <AboutItemTitle>ЯКІСНІ МАТЕРІАЛИ</AboutItemTitle>
+            <AboutItemText>
+              Виготовлений з нержавіючої сталі марки AISI 304 і якісної
+              поліацеталі на нашому виробництві.
+            </AboutItemText>
+          </AboutItem>
+          <AboutItem>
+            <Stars color="#fff" width={'50%'} />
+            <AboutItemTitle>КОМФОРТ ПАЛІННЯ</AboutItemTitle>
+            <AboutItemText>
+              Легка, приємна тяга. Унікальна продувка за 1-2 рази.
+            </AboutItemText>
+          </AboutItem>
+          <AboutItem>
+            <Gear color="#fff" width={'50%'} />
+            <AboutItemTitle>ЗРУЧНІСТЬ ЗБІРКИ</AboutItemTitle>
+            <AboutItemText>
+              Відсутня різьба в полаіцеталі. Порт на щільній притирці. Шахта
+              розбірна.
+            </AboutItemText>
+          </AboutItem>
+          <AboutItem>
+            <Moon color="#fff" width={'50%'} />
+            <AboutItemTitle>ДИЗАЙН</AboutItemTitle>
+            <AboutItemText>
+              Великий вибір моделей, декоративних накладок та колб. Блюдце
+              Орбіта унікальної форми.
+            </AboutItemText>
+          </AboutItem>
+          <AboutItem>
+            <Biceps color="#fff" width={'50%'} />
+            <AboutItemTitle>НАДІЙНІСТЬ</AboutItemTitle>
+            <AboutItemText>
+              TIAGA HOOKAH - український виробник кальянів, на ринку з 2018
+              року.
+            </AboutItemText>
+          </AboutItem>
+          <AboutItem>
+            <Coins color="#fff" width={'50%'} />
+            <AboutItemTitle>ДОСТУНА ЦІНА</AboutItemTitle>
+            <AboutItemText>Топова якість за вигідну ціну.</AboutItemText>
+          </AboutItem>
+        </Aboutlist>
+      </Container>
+    </Section>
+  );
+};
+
+export default About;
