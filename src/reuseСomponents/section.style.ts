ade826e7c53ab46d548358eaf0f6d036024fbf1d
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-06-23 17:36:00 +0300

    Create section.style.ts

---
+++ src/reuseСomponents/section.style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+type SectionProps = {
+  bgc?: string;
+};
+
+export const Section = styled.section<SectionProps>`
+  width: 100%;
+  min-height: 100vh;
+
+  background-color: ${props => props.bgc};
+
+  padding-top: 20px;
+  padding-bottom: 20px;
+
+  @media screen and (min-width: 1440px) {
+    padding-top: 40px;
+    padding-bottom: 40px;
+  }
+`;
