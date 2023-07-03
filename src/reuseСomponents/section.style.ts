869b28f7772b3d14836fb0312756ab97cfc55fc8
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-03 16:23:53 +0300

    Update section.style.ts

--- src/reuseСomponents/section.style.ts
+++ src/reuseСomponents/section.style.ts
@@ -5,15 +5,16 @@ type SectionProps = {
 };
 
 export const Section = styled.section<SectionProps>`
+  scroll-snap-align: start;
   width: 100%;
 
   background-color: ${props => props.bgc};
 
-  padding-top: 20px;
+  padding-top: 60px;
   padding-bottom: 20px;
 
   @media screen and (min-width: 1440px) {
-    padding-top: 40px;
+    padding-top: 80px;
     padding-bottom: 40px;
   }
 `;
