84397fea9eb792c5d6bd765d8b4304977a301e70
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-03 16:23:55 +0300

    Update home.tsx

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -3,16 +3,25 @@ import Hero from '../../components/hero/hero';
 import About from '../../components/about/about';
 import Gallery from '../../components/gallery/gallery';
 import TovarGallery from '../../components/tovarGallety/tovarGallety';
+import Contacts from '../../components/contacts/contacts';
 
 type HomeProps = {};
 
 const Home: FC<HomeProps> = props => {
   return (
-    <div style={{ scrollBehavior: 'smooth' }}>
+    <div
+      style={{
+        scrollBehavior: 'smooth',
+        scrollSnapType: 'y mandatory',
+        overflowY: 'scroll',
+        height: '100vh',
+      }}
+    >
       <Hero />
       <Gallery />
       <TovarGallery />
       <About />
+      <Contacts />
     </div>
   );
 };
