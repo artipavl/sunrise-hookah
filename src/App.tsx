bff70e8bb382ceb929604a943dbfde1ac2eed527
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-06 17:30:33 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
 import Home from './pages/home/home';
-import Heder from './components/header/heder';
-import SocialLinks from './components/socialLinks/socialLinks';
-import Footer from './components/footer/footer';
 import Product from './pages/product/product';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
+  /* display: flex;
+  flex-direction: column; */
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Heder />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/flasks" element={<Product title="Колби" />} />
       </Routes>
-      <SocialLinks />
-      <Footer />
     </Wrapper>
   );
 }
