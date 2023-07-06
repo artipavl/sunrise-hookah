6263ea32a371a56867e2d2d42a2ecdb2eea1b3f4
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-06 17:30:41 +0300

    Update container.style.ts

--- src/reuseСomponents/container.style.ts
+++ src/reuseСomponents/container.style.ts
@@ -8,6 +8,7 @@ export const Container = styled.div<ContainerProps>`
   padding-left: 20px;
   padding-right: 20px;
   max-width: 1440px;
+  height: 100%;
 
   ${props =>
     props.flex === 'center' &&
@@ -16,12 +17,7 @@ export const Container = styled.div<ContainerProps>`
   justify-content: center;
   align-items: center;`}
 
-  min-height: calc(100vh - 80px);
-  /* max-height: calc(100vh - 80px); */
-
   @media screen and (min-width: 1440px) {
     margin: auto;
-    min-height: calc(100vh - 120px);
-    /* max-height: calc(100vh - 120px); */
   }
 `;
