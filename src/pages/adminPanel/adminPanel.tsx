a8f1e335ed3fda7bd98b65c205437469886eba80
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-07-11 19:06:04 +0300

    Create adminPanel.tsx

---
+++ src/pages/adminPanel/adminPanel.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+
+type AdminPanelProps = {};
+
+const AdminPanel: FC<AdminPanelProps> = props => {
+  return (
+    <>
+      <header>header</header>
+      <main>main</main>
+    </>
+  );
+};
+
+export default AdminPanel;
