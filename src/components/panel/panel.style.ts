31ba1eac04d8c2609bda23805a719b741ff9fd51
Kindual <110603179+Kindual@users.noreply.github.com>
2023-07-23 20:14:54 +0300

    styled troshku

---
+++ src/components/panel/panel.style.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const PanelBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const SearchPanel = styled.form`
+    
+`;
+
+export const FormInput = styled.input`
+    line-height: 22px;
+    padding: 5px 15px;
+
+    border-radius: 25px;
+    border: 2px solid #D1D1D1;
+    background: #FFF;
+`;
+
+export const FormButton = styled.button`
+    color: #C5C7CD;
+    position: relative;
+    left: -30px;
+    top: 2px;
+`;
+
+export const Button = styled.button`
+    color: #880000; 
+`;
+
---
+++ src/components/tovarsByType/tovarsByType.tsx
@@ -0,0 +1,65 @@
+import React, { FC, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { TovarBox, TovarInfo, TovarItem, TovarItemListPropItem, TovarItemListProps, TovarList } from './tovarsByType.style';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { fetchTovarsByTypes } from '../../redux/tovars/tovarsOperations';
+import { selectTypes } from '../../redux/types/slice';
+import { selectTovars, selectTovarsLoading } from '../../redux/tovars/slice';
+import Panel from '../panel/panel';
+import { AiOutlineDelete, AiOutlineDollarCircle, AiOutlineForm, AiOutlineGold, AiOutlineRise } from 'react-icons/ai';
+
+
+type AdminPanelProps = {};
+
+
+const TovarsByType: FC<AdminPanelProps> = props => {
+
+    const params = useParams();
+
+    const types = useAppSelector(selectTypes);
+    const start = useAppSelector(selectTovarsLoading);
+    const tovars = useAppSelector(selectTovars)
+
+    const type = types.find(type => type.eu === params.id?.toLowerCase());
+    const AppDispatch = useAppDispatch();
+    useEffect(() => {
+        if (type?.eu) {
+            AppDispatch(fetchTovarsByTypes(type.eu));
+        }
+    }, [AppDispatch, type]);
+
+    if (!start) {
+        return <TovarBox >Loading...        </TovarBox>
+    }
+    console.log(tovars)
+
+    return (
+        <TovarBox>
+            <Panel />
+
+            {/* {type?.ua} */}
+            <TovarList>
+                {tovars.map(tovar =>
+                    <li key={tovar.id}>
+                        <TovarItem>
+                            <span>{tovar.name.ua}</span>
+                            <TovarInfo>
+                                <TovarItemListProps>
+                                    <TovarItemListPropItem><AiOutlineDollarCircle /> {tovar.cost}</TovarItemListPropItem>
+                                    <TovarItemListPropItem><AiOutlineRise />{tovar.popularity}</TovarItemListPropItem>
+                                    <TovarItemListPropItem><AiOutlineGold />{tovar.quantity}</TovarItemListPropItem>
+                                </TovarItemListProps>
+                                <TovarItemListProps>
+                                    <TovarItemListPropItem><AiOutlineForm /></TovarItemListPropItem>
+                                    <TovarItemListPropItem><AiOutlineDelete /></TovarItemListPropItem>
+                                </TovarItemListProps>
+
+                            </TovarInfo>
+                        </TovarItem>
+                    </li>)}
+            </TovarList>
+        </TovarBox>
+    );
+};
+
+export default TovarsByType;
