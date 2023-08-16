248edf80defcfbdf82e6f91950d9f37f48e12b55
Kindual <110603179+Kindual@users.noreply.github.com>
2023-08-16 18:58:13 +0300

    base fetch tovar by id

--- src/pages/tovarPage/tovarPage.tsx
+++ src/pages/tovarPage/tovarPage.tsx
@@ -1,35 +1,49 @@
-import React, { FC, useMemo } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Container } from '../../reuseСomponents/container.style';
 import Heder from '../../components/header/heder';
 import { SectionTovar } from './tovarPage.style';
 import { useParams } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
 import { selectTovars } from '../../redux/tovars/slice';
+import axios from 'axios';
+import Tovar from '../../Types/tovar';
+
 
 type TovarPageProps = {};
 
 const TovarPage: FC<TovarPageProps> = (props) => {
-    const { id } = useParams();
-    console.log(id)
-
+    const [tovar, setTovar] = useState<Tovar | null>(null);
     const tovars = useAppSelector(selectTovars);
-    
-    const tovar = useMemo(() => {
-        if (id) {
-            return tovars.find(item => item.id === id)
+    const { id } = useParams();
+
+    useEffect(() => {
+        const get = async (id: string | undefined, tovars: Tovar[]) => {
+            if (id) {
+                const tovar = tovars.find(item => item.id === id);
+                if (!tovar) {
+                    try {
+                        const { data } = await axios.get(`/tovar/${id}`);
+                        setTovar(data as Tovar) 
+                    } catch (error) {
+                    }
+                }
+                tovar && setTovar(tovar) 
+            }
         }
-    }, [id, tovars])
-    console.log(tovar)
+
+        get(id, tovars)
+    }, [id, tovars]);
 
     if (!tovar) {
-        return <div>Fuch you</div>
+        return <div>Fuck you</div>
     }
+
     return (
-        
+
         <SectionTovar>
             <Heder></Heder>
             <Container>
-                <h1>{tovar?.name.ua}</h1>
+                {/* <h1>{tovar?.name.ua}</h1> */}
                 <p></p>
                 <form>
                     <input type='number' />
