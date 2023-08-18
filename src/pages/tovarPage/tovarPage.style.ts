00e4c9dd2d0feb12a4fe9277e2557744186fb141
Kindual <110603179+Kindual@users.noreply.github.com>
2023-08-18 16:07:06 +0300

    base wrapp

--- src/pages/tovarPage/tovarPage.style.ts
+++ src/pages/tovarPage/tovarPage.style.ts
@@ -1,7 +1,83 @@
 import styled from 'styled-components'; 
 import { Section } from '../../reuseСomponents/section.style';
+import { Container } from '../../reuseСomponents/container.style';
 
 export const SectionTovar = styled(Section)`
   display: block;
-  padding-top: 0;
+  padding-top: 0; 
+`;
+
+export const ContainerTovar = styled(Container)`
+  display: flex;
+  /* justify-content: center; */
+  align-items: center;
+
+  background-color: #fff;
+  padding: 20px;
+  gap: 30px;
+  height: auto;
+`;
+
+export const MainInfoBox = styled.div`
+  
+`;
+
+export const ButtonCase = styled.button`
+  background-color: #FFD700;
+  padding: 8px 28px;
+  border-radius: 8px;
+`;
+
+export const InfoBox = styled.div` 
+`;
+
+export const FormBox = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const InputQuantity = styled.input`
+  width: 100px;
+`;
+
+export const Gallery = styled.div`
+  
+`;
+export const ImgBox = styled.div`
+  width: 400px;
+  height: 500px;
+  object-fit: scale-down;
+`;
+
+export const Img = styled.img`
+  object-fit: none;
+  width: 100%;
+  height: 100%;
+
+  background-color: #dcdcdc;
+`;
+
+export const SubinfoBox = styled.div`
+  
+`;
+
+export const MiniGallery = styled.ul`
+  margin-top: 10px;
+  display: flex; 
+  gap: 10px;
+`;
+
+export const MiniGalleryItem = styled.li`
+  width: 100px;
+  height: 100px;
+`;
+
+export const BtnImg = styled.button`
+  height: 100%;
+  width: 100%;
+`;
+
+export const MiniImg = styled.img`
+  width: 100%;
+  object-fit: cover;
 `;
--- src/pages/tovarPage/tovarPage.tsx
+++ src/pages/tovarPage/tovarPage.tsx
@@ -1,7 +1,6 @@
 import React, { FC, useState, useEffect } from 'react';
-import { Container } from '../../reuseСomponents/container.style';
 import Heder from '../../components/header/heder';
-import { SectionTovar } from './tovarPage.style';
+import { BtnImg, ButtonCase, ContainerTovar, FormBox, Gallery, Img, ImgBox, InfoBox, InputQuantity, MainInfoBox, MiniGallery, MiniGalleryItem, MiniImg, SectionTovar, SubinfoBox } from './tovarPage.style';
 import { useParams } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
 import { selectTovars } from '../../redux/tovars/slice';
@@ -15,6 +14,7 @@ const TovarPage: FC<TovarPageProps> = (props) => {
     const [tovar, setTovar] = useState<Tovar | null>(null);
     const tovars = useAppSelector(selectTovars);
     const { id } = useParams();
+    const [imgUrl, setImgUrl] = useState(0);
 
     useEffect(() => {
         const get = async (id: string | undefined, tovars: Tovar[]) => {
@@ -23,11 +23,11 @@ const TovarPage: FC<TovarPageProps> = (props) => {
                 if (!tovar) {
                     try {
                         const { data } = await axios.get(`/tovar/${id}`);
-                        setTovar(data as Tovar) 
+                        setTovar(data as Tovar)
                     } catch (error) {
                     }
                 }
-                tovar && setTovar(tovar) 
+                tovar && setTovar(tovar)
             }
         }
 
@@ -39,17 +39,38 @@ const TovarPage: FC<TovarPageProps> = (props) => {
     }
 
     return (
-
         <SectionTovar>
             <Heder></Heder>
-            <Container>
-                {/* <h1>{tovar?.name.ua}</h1> */}
-                <p></p>
-                <form>
-                    <input type='number' />
-                    <button type="submit"></button>
-                </form>
-            </Container>
+            <ContainerTovar>
+                <InfoBox>
+                    <Gallery>
+                        <ImgBox>
+                            <Img src={tovar.fotos[imgUrl]} alt="foto hookah" />
+                        </ImgBox>
+                        <MiniGallery>
+                            {tovar?.fotos.map((foto, index) =>
+                                <MiniGalleryItem key={foto}>
+                                    <BtnImg type='button' onClick={() => setImgUrl(index)}>
+                                        <MiniImg src={foto} alt='фото кальяну' />
+                                    </BtnImg>
+                                </MiniGalleryItem>
+                            )}
+                        </MiniGallery>
+                    </Gallery>
+                    <SubinfoBox>
+                        <p>{tovar.description.ua}</p>
+                        <p>{tovar.parameters.ua}</p>
+                    </SubinfoBox>
+                </InfoBox>
+                <MainInfoBox>
+                    <h1>{tovar?.name.ua}</h1>
+                    <p>{tovar?.cost}</p>
+                    <FormBox>
+                        <InputQuantity type='number' min={1} value={1} />
+                        <ButtonCase type="submit">Додати в кошик</ButtonCase>
+                    </FormBox>
+                </MainInfoBox>
+            </ContainerTovar>
         </SectionTovar >
     )
 }
