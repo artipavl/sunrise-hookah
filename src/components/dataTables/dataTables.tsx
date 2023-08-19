20559ee2fde633cbaa3a5b3fa32ec1bff3dbfe8c
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-19 15:41:51 +0300

    update

--- src/components/dataTables/dataTables.tsx
+++ src/components/dataTables/dataTables.tsx
@@ -1,42 +1,79 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import {
   Column,
   Columns,
   Rows,
   Table,
   Tbody,
-  Tfoot,
-  TfootButton,
-  TfootTh,
-  TfootTr,
+  // Tfoot,
+  // TfootButton,
+  // TfootTh,
+  // TfootTr,
   Thead,
+  DeleteButton,
+  ModalBox,
+  ModalDeleteTitle,
+  ModalDeleteButtonsBox,
+  ModalDeleteButton,
 } from './dataTables.style';
+import { AiOutlineRest } from 'react-icons/ai';
+import Modal from '../modal/modal';
+import { Admin } from '../../Types/typesAuth';
 
 type DataTablesProps = {
   rows: string[];
+  rowsTitle: string[];
   columns: any[];
+  remove: (id: string) => void;
 };
 
-const DataTables: FC<DataTablesProps> = ({ rows, columns }) => {
+const DataTables: FC<DataTablesProps> = ({
+  rows,
+  columns,
+  rowsTitle,
+  remove,
+}) => {
+  const [current, setCurrent] = useState<Admin | null>(null);
+  const [deleteOpen, setDeleteOpen] = useState<boolean>();
+
   return (
-    <Table cellSpacing={0}>
-      <Thead>
-        <tr>
-          {rows.map(row => (
-            <Rows key={row}>{row}</Rows>
-          ))}
-        </tr>
-      </Thead>
-      <Tbody>
-        {columns.map((column, index) => (
-          <Columns key={index}>
-            {rows.map((row, rowIndex) => (
-              <Column key={`${index}-${rowIndex}`}>{column[row]}</Column>
+    <>
+      <Table cellSpacing={0}>
+        <Thead>
+          <tr>
+            {rows.map((row, index) => (
+              <Rows key={row} delete={Boolean(row === 'delete')}>
+                {rowsTitle[index]}
+              </Rows>
             ))}
-          </Columns>
-        ))}
-      </Tbody>
-      <Tfoot>
+          </tr>
+        </Thead>
+        <Tbody>
+          {columns.map((column, index) => (
+            <Columns key={index}>
+              {rows.map((row, rowIndex) =>
+                row === 'delete' ? (
+                  <Column
+                    key={`${index}-${rowIndex}`}
+                    style={{ textAlign: 'center' }}
+                  >
+                    <DeleteButton
+                      onClick={() => {
+                        setCurrent(column);
+                        setDeleteOpen(true);
+                      }}
+                    >
+                      <AiOutlineRest />
+                    </DeleteButton>
+                  </Column>
+                ) : (
+                  <Column key={`${index}-${rowIndex}`}>{column[row]}</Column>
+                )
+              )}
+            </Columns>
+          ))}
+        </Tbody>
+        {/* <Tfoot>
         <TfootTr>
           <TfootTh>
             <TfootButton type="button">Previous</TfootButton>
@@ -48,8 +85,34 @@ const DataTables: FC<DataTablesProps> = ({ rows, columns }) => {
             <TfootButton type="button">Next</TfootButton>
           </TfootTh>
         </TfootTr>
-      </Tfoot>
-    </Table>
+      </Tfoot> */}
+      </Table>
+      {deleteOpen && (
+        <Modal openBasket={() => setDeleteOpen(false)}>
+          <ModalBox>
+            <ModalDeleteTitle>Дійсно видалити</ModalDeleteTitle>
+            <ModalDeleteButtonsBox>
+              <ModalDeleteButton
+                type="button"
+                onClick={() => {
+                  current && remove(current.id);
+                  setDeleteOpen(false);
+                  setCurrent(null);
+                }}
+              >
+                Так
+              </ModalDeleteButton>
+              <ModalDeleteButton
+                type="button"
+                onClick={() => setDeleteOpen(false)}
+              >
+                Ні
+              </ModalDeleteButton>
+            </ModalDeleteButtonsBox>
+          </ModalBox>
+        </Modal>
+      )}
+    </>
   );
 };
 
