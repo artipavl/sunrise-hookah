0b98ba1a783f3d470b89dcd6b2b6365975ca649e
Kindual <110603179+Kindual@users.noreply.github.com>
2023-08-19 15:58:25 +0300

    styles

--- src/pages/tovarPage/tovarPage.style.ts
+++ src/pages/tovarPage/tovarPage.style.ts
@@ -1,21 +1,25 @@
-import styled from 'styled-components'; 
+import styled from 'styled-components';
 import { Section } from '../../reuseСomponents/section.style';
 import { Container } from '../../reuseСomponents/container.style';
 
 export const SectionTovar = styled(Section)`
   display: block;
   padding-top: 0; 
+  color: aqua; 
+
 `;
 
 export const ContainerTovar = styled(Container)`
   display: flex;
   /* justify-content: center; */
   align-items: center;
+  flex-direction: column;
 
   background-color: #fff;
   padding: 20px;
   gap: 30px;
   height: auto;
+  width: 100%;
 `;
 
 export const MainInfoBox = styled.div`
@@ -29,6 +33,7 @@ export const ButtonCase = styled.button`
 `;
 
 export const InfoBox = styled.div` 
+  max-width: fit-content;
 `;
 
 export const FormBox = styled.form`
@@ -41,16 +46,15 @@ export const InputQuantity = styled.input`
 `;
 
 export const Gallery = styled.div`
-  
+  width: 100%;
 `;
 export const ImgBox = styled.div`
-  width: 400px;
-  height: 500px;
-  object-fit: scale-down;
+  width: 100%;
+  height: 300px; 
 `;
 
 export const Img = styled.img`
-  object-fit: none;
+  object-fit: contain;
   width: 100%;
   height: 100%;
 
@@ -58,7 +62,9 @@ export const Img = styled.img`
 `;
 
 export const SubinfoBox = styled.div`
-  
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 
 export const MiniGallery = styled.ul`
@@ -68,8 +74,8 @@ export const MiniGallery = styled.ul`
 `;
 
 export const MiniGalleryItem = styled.li`
-  width: 100px;
-  height: 100px;
+  height: 60px;
+  width: 60px;
 `;
 
 export const BtnImg = styled.button`
@@ -78,6 +84,19 @@ export const BtnImg = styled.button`
 `;
 
 export const MiniImg = styled.img`
+  height: 100%;
   width: 100%;
   object-fit: cover;
+  opacity: 0.5;
+  border: 0px solid #fff;
+    
+  transition: ease 600ms opacity, ease 600ms border;
+  
+  &:hover,
+  &:focus {
+    opacity: 1;
+    border: 1px solid red;
+    
+    transition: ease 600ms opacity, ease 600ms border;
+  }
 `;
