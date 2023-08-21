100f5fe57517044847c05135bbfc276a8d1ea75d
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-21 16:34:50 +0300

    Update Orders.tsx

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -1,11 +1,15 @@
 import React, { FC } from 'react';
+import OrdersDataTable from '../../components/ordersDataTable/ordersDataTable';
+import { Order } from '../../Types/order';
 
 type OrdersProps = {};
 
 const Orders: FC<OrdersProps> = props => {
+  const Data: Order[] = [];
   return (
     <div>
       <p>Orders</p>
+      <OrdersDataTable data={Data} />
     </div>
   );
 };
