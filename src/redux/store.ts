49be2cc95fc4fb66e34bb43522fc1efe485b141e
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-28 12:29:54 +0300

    add Reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,5 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { useDispatch } from 'react-redux';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+	persistStore,
+	persistReducer,
+	FLUSH,
+	REHYDRATE,
+	PAUSE,
+	PERSIST,
+	PURGE,
+	REGISTER,
+} from 'redux-persist';
+
+import storage from 'redux-persist/lib/storage';
+
 import selectTovars from './tovars/slice';
 import typesSlice from './types/slice';
 import basketSlice from './basket/basketSlice';
@@ -8,21 +20,43 @@ import adminsSlice from './adminList/slice';
 import ordersSlice from './orders/slice';
 import { authSlice } from './auth/authReducer';
 
-const store = configureStore({
-	reducer: {
-		tovars: selectTovars,
-		types: typesSlice,
-		basket: basketSlice,
-		feedbacks: feedbackSlice,
-		auth: authSlice.reducer,
-		admins: adminsSlice,
-		orders: ordersSlice,
-	},
+const persistTovars = {
+	key: 'tovars',
+	storage,
+};
+
+const persistBasket = {
+	key: 'basket',
+	storage,
+};
+const persistTypes = {
+	key: 'types',
+	storage,
+};
+
+const rootReducer = combineReducers({
+	tovars: persistReducer(persistTovars, selectTovars),
+	basket: persistReducer(persistBasket, basketSlice),
+	types: persistReducer(persistTypes, typesSlice),
+
+	feedbacks: feedbackSlice,
+	auth: authSlice.reducer,
+	admins: adminsSlice,
+	orders: ordersSlice,
 });
+
+export const store = configureStore({
+	reducer: rootReducer,
+	middleware: getDefaultMiddleware =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
-
-export default store;
