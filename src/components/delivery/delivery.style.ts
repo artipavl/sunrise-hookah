d29a260b0d2436823701a12a9104379e45e545d8
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-28 11:31:57 +0300

    update delivery

--- src/components/delivery/delivery.style.ts
+++ src/components/delivery/delivery.style.ts
@@ -1,54 +1,85 @@
 import styled from 'styled-components';
 
-export const Input = styled.input``;
+export const Box = styled.div`
+	display: flex;
+	gap: 30px;
+`;
 
 export const Lable = styled.label`
-  position: relative;
+	position: relative;
+	color: white;
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
 `;
 
 export const List = styled.ul`
-  position: absolute;
-  padding-block: 5px;
-  border-radius: 3px;
-  width: max-content;
-  background-color: #fff;
+	position: absolute;
+	left: 0;
+	top: calc(100% + 10px);
+	padding-block: 5px;
+	border-radius: 3px;
+	width: max-content;
+	background-color: #fff;
+	color: #000;
 `;
 export const Item = styled.li`
-  padding-inline: 5px;
-  cursor: pointer;
+	padding-inline: 5px;
+	cursor: pointer;
 
-  :hover,
-  :focus {
-    background-color: #fafafa;
-  }
+	:hover,
+	:focus {
+		background-color: #fafafa;
+	}
 `;
 
 export const WarehousesList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  margin-top: 30px;
-  color: #fff;
+	/* color: #000; */
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	margin-top: 30px;
+	color: #fff;
 `;
 
 export const WarehousesItem = styled.li`
-  display: flex;
-  align-items: center;
-  cursor: pointer;
+	display: flex;
+	align-items: center;
+	cursor: pointer;
 
-  :hover,
-  :focus {
-    color: red;
-  }
+	:hover,
+	:focus {
+		color: red;
+	}
 
-  ::before {
-    content: ' ';
-    display: block;
-    width: 10px;
-    height: 10px;
-    background-color: #fff;
-    border: 1px solid #aaa;
-    border-radius: 50%;
-    margin-right: 5px;
-  }
+	::before {
+		content: ' ';
+		display: block;
+		width: 10px;
+		height: 10px;
+		background-color: #fff;
+		border: 1px solid #aaa;
+		border-radius: 50%;
+		margin-right: 5px;
+	}
+`;
+
+export const Input = styled.input`
+	position: relative;
+	display: inline-block;
+	/* width: 100%; */
+	background: #ffffff;
+	border: 1px solid #ffffff;
+	/* border-radius: 90px; */
+
+	padding: 10px 20px;
+
+	::placeholder {
+		font-weight: 400;
+		font-size: 16px;
+		line-height: 20px;
+		/* identical to box height */
+
+		color: #484848;
+	}
 `;
--- src/components/delivery/delivery.tsx
+++ src/components/delivery/delivery.tsx
@@ -1,125 +1,128 @@
 import axios from 'axios';
 import React, { FC, useState, useEffect } from 'react';
 import {
-  Address,
-  SearchSettlementsType,
-  Warehous,
+	Address,
+	SearchSettlementsType,
+	Warehous,
 } from '../../Types/novaposhta';
 import {
-  Input,
-  Item,
-  Lable,
-  List,
-  WarehousesItem,
-  WarehousesList,
+	Box,
+	Input,
+	Item,
+	Lable,
+	List,
+	WarehousesItem,
+	WarehousesList,
 } from './delivery.style';
 
 type DeliveryProps = {
-  submit: (values: Warehous) => void;
+	submit: (values: Warehous) => void;
 };
 
 const Delivery: FC<DeliveryProps> = ({ submit }) => {
-  const [towns, setTowns] = useState<SearchSettlementsType>({
-    TotalCount: 0,
-    Addresses: [],
-  });
-  const [town, setTown] = useState<string>('');
-  const [curentAdress, setCurentAdress] = useState<Address>();
+	const [towns, setTowns] = useState<SearchSettlementsType>({
+		TotalCount: 0,
+		Addresses: [],
+	});
+	const [town, setTown] = useState<string>('');
+	const [curentAdress, setCurentAdress] = useState<Address>();
 
-  const [warehouses, setWarehouses] = useState<Warehous[]>();
-  const [warehouseNumber, setWarehouseNumber] = useState<number>(0);
+	const [warehouses, setWarehouses] = useState<Warehous[]>();
+	const [warehouseNumber, setWarehouseNumber] = useState<number>(0);
 
-  useEffect(() => {
-    async function getTowns() {
-      const res = await axios.post(
-        'https://sunrise-hookah-api.onrender.com/novaposhta/searchSettlements',
-        {
-          Page: 1,
-          CityName: town,
-          Limit: 5,
-        }
-      );
-      if (res.data.data[0]) {
-        setTowns(res.data.data[0] as SearchSettlementsType);
-      }
-    }
+	useEffect(() => {
+		async function getTowns() {
+			const res = await axios.post(
+				'https://sunrise-hookah-api.onrender.com/novaposhta/searchSettlements',
+				{
+					Page: 1,
+					CityName: town,
+					Limit: 5,
+				}
+			);
+			if (res.data.data[0]) {
+				setTowns(res.data.data[0] as SearchSettlementsType);
+			}
+		}
 
-    if (town !== curentAdress?.Present && town !== '') {
-      getTowns();
-    }
-  }, [curentAdress?.Present, town]);
+		if (town !== curentAdress?.Present && town !== '') {
+			getTowns();
+		}
+	}, [curentAdress?.Present, town]);
 
-  useEffect(() => {
-    async function getWarehouses(curentAdress: Address) {
-      const params = {
-        Page: 1,
-        CityName: curentAdress.MainDescription,
-        Limit: 5,
-        CityRef: curentAdress.DeliveryCity,
-      };
-      const res = await axios.post(
-        'https://sunrise-hookah-api.onrender.com/novaposhta/getWarehouses',
-        warehouseNumber > 0
-          ? { ...params, WarehouseId: warehouseNumber }
-          : { ...params }
-      );
-      if (res.data.data) {
-        setWarehouses(res.data.data);
-      }
-    }
-    if (curentAdress) {
-      getWarehouses(curentAdress);
-    }
-  }, [curentAdress, warehouseNumber]);
-  return (
-    <>
-      <div>
+	useEffect(() => {
+		async function getWarehouses(curentAdress: Address) {
+			const params = {
+				Page: 1,
+				CityName: curentAdress.MainDescription,
+				Limit: 5,
+				CityRef: curentAdress.DeliveryCity,
+			};
+			const res = await axios.post(
+				'https://sunrise-hookah-api.onrender.com/novaposhta/getWarehouses',
+				warehouseNumber > 0
+					? { ...params, WarehouseId: warehouseNumber }
+					: { ...params }
+			);
+			if (res.data.data) {
+				setWarehouses(res.data.data);
+			}
+		}
+		if (curentAdress) {
+			getWarehouses(curentAdress);
+		}
+	}, [curentAdress, warehouseNumber]);
+	return (
+		<>
+			<Box>
+				<Lable>
+					Місто
+					<Input
+						type="text"
+						value={town}
+						onChange={e => setTown(e.target.value)}
+						placeholder={'Київ'}
+					/>
+					{towns.TotalCount > 0 && town !== curentAdress?.Present && (
+						<List>
+							{towns.Addresses.map(address => (
+								<Item
+									key={address.Ref}
+									onClick={() => {
+										setCurentAdress(address);
+										setTown(address.Present);
+										console.log(address);
+									}}
+								>
+									{address.Present}
+								</Item>
+							))}
+						</List>
+					)}
+				</Lable>
         <Lable>
-          <Input
-            type="text"
-            value={town}
-            onChange={e => setTown(e.target.value)}
-            placeholder={'Київ'}
-          />
-          {towns.TotalCount > 0 && town !== curentAdress?.Present && (
-            <List>
-              {towns.Addresses.map(address => (
-                <Item
-                  key={address.Ref}
-                  onClick={() => {
-                    setCurentAdress(address);
-                    setTown(address.Present);
-                    console.log(address);
-                  }}
-                >
-                  {address.Present}
-                </Item>
-              ))}
-            </List>
-          )}
-        </Lable>
-        <label>
-          <input
-            type="number"
-            value={warehouseNumber}
-            min={0}
-            onChange={e => setWarehouseNumber(Number(e.target.value))}
-          />
-        </label>
-      </div>
-      <WarehousesList>
-        {warehouses &&
-          warehouses.map(warehous => (
-            <WarehousesItem
-              key={warehous.Number}
-              onClick={() => submit(warehous)}
-            >
-              {warehous.Description}
-            </WarehousesItem>
-          ))}
-      </WarehousesList>
-    </>
-  );
+          № Відділення
+					<Input
+						type="number"
+						value={warehouseNumber}
+						min={0}
+						onChange={e => setWarehouseNumber(Number(e.target.value))}
+					/>
+				</Lable>
+			</Box>
+			<WarehousesList>
+				{warehouses &&
+					warehouses.map(warehous => (
+						<WarehousesItem
+							key={warehous.Number}
+							onClick={() => submit(warehous)}
+						>
+							{warehous.Description}
+						</WarehousesItem>
+					))}
+			</WarehousesList>
+		</>
+	);
 };
 
 export default Delivery;
