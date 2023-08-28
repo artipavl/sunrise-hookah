a28af1aa4dcfa5ba1b745b3b61e3a56d0383ca4f
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-28 11:56:34 +0300

    Update basket.tsx

--- src/components/basket/basket.tsx
+++ src/components/basket/basket.tsx
@@ -1,98 +1,110 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import {
-  BasketSection,
-  BasketBox,
-  BasketHeader,
-  BasketCloseButton,
-  BasketList,
-  Buy,
-  BasketSum,
-  BasketItam,
-  BasketItamImage,
-  BasketItamInput,
-  BasketItamInformation,
-  BasketItamDelete,
+	BasketSection,
+	BasketBox,
+	BasketHeader,
+	BasketCloseButton,
+	BasketList,
+	Buy,
+	BasketSum,
+	BasketItam,
+	BasketItamImage,
+	BasketItamInput,
+	BasketItamInformation,
+	BasketItamDelete,
 } from './basket.style';
 import { createPortal } from 'react-dom';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { selectBasket } from '../../redux/basket/basketSelectors';
 import {
-  deleteFromBasket,
-  updateQuantity,
+	deleteFromBasket,
+	updateQuantity,
 } from '../../redux/basket/basketSlice';
-import { AiFillDelete } from 'react-icons/ai';
+import { AiFillDelete, AiOutlineRight } from 'react-icons/ai';
 
 type BasketProps = {
-  openBasket: () => void;
+	openBasket: () => void;
 };
 
 const portal = document.getElementById('portal') as HTMLElement;
 
 export const Basket: FC<BasketProps> = ({ openBasket }) => {
-  const tovars = useAppSelector(selectBasket);
+	const tovars = useAppSelector(selectBasket);
 
-  const dispatch = useAppDispatch();
+	const dispatch = useAppDispatch();
 
-  return createPortal(
-    <BasketSection onClick={e => e.target === e.currentTarget && openBasket()}>
-      <BasketBox>
-        <BasketHeader>
-          <BasketCloseButton type="button" onClick={() => openBasket()}>
-            {'>'}
-          </BasketCloseButton>
-          <h2>Кошик</h2>
-        </BasketHeader>
-        <BasketList>
-          {tovars.map(tovar => (
-            <BasketItam key={tovar.id}>
-              <BasketItamImage
-                src={
-                  tovar.fotos[0]
-                    ? tovar.fotos[0]
-                    : 'https://kor.ill.in.ua/m/610x385/2722809.jpg'
-                }
-                alt={tovar.nameEN}
-              />
-              <BasketItamInformation>
-                <span>{tovar.nameUKR}</span>
-                <span>{tovar.cost} грн.</span>
+	// useEffect(() => {
+	//   document.body.style.overflow = "hiaden"
 
-                <BasketItamInput
-                  type="number"
-                  name="quantity"
+	// }, []);
+	useEffect(() => {
+		document.body.style.overflow = 'hidden';
+
+		return () => {
+			document.body.style.overflow = 'auto';
+		};
+	}, []);
+	return createPortal(
+		<BasketSection onClick={e => e.target === e.currentTarget && openBasket()}>
+			<BasketBox>
+				<BasketHeader>
+					<BasketCloseButton type="button" onClick={() => openBasket()}>
+						<AiOutlineRight />
+					</BasketCloseButton>
+					<h2>Кошик</h2>
+				</BasketHeader>
+				<BasketList>
+					{tovars.map(tovar => (
+						<BasketItam key={tovar.id}>
+							<BasketItamImage
+								src={
+									tovar.fotos[0]
+										? tovar.fotos[0]
+										: 'https://kor.ill.in.ua/m/610x385/2722809.jpg'
+								}
+								alt={tovar.nameEN}
+							/>
+							<BasketItamInformation>
+								<span>{tovar.nameUKR}</span>
+								<span>{tovar.cost} грн.</span>
+
+								<BasketItamInput
+									type="number"
+									name="quantity"
                   id="quantity"
-                  value={tovar.baskeQuantity}
-                  onChange={e =>
-                    dispatch(
-                      updateQuantity({
-                        id: tovar.id,
-                        baskeQuantity: Number(e.target.value),
-                      })
-                    )
-                  }
-                />
-              </BasketItamInformation>
-              <BasketItamDelete
-                id="delete"
-                type="button"
-                onClick={() => dispatch(deleteFromBasket(tovar.id))}
-              >
-                <AiFillDelete />
-              </BasketItamDelete>
-            </BasketItam>
-          ))}
-        </BasketList>
-        <BasketSum>
-          <samp>Сума: </samp>
-          <samp>
-            {tovars.reduce((a, b) => a + b.cost * b.baskeQuantity, 0)} грн
-          </samp>
-        </BasketSum>
-        <Buy to="/checkout">Оформити</Buy>
-      </BasketBox>
-    </BasketSection>,
-    portal
-  );
+                  min={1}
+									value={tovar.baskeQuantity}
+									onChange={e =>
+										dispatch(
+											updateQuantity({
+												id: tovar.id,
+												baskeQuantity: Number(e.target.value),
+											})
+										)
+									}
+								/>
+							</BasketItamInformation>
+							<BasketItamDelete
+								id="delete"
+								type="button"
+								onClick={() => dispatch(deleteFromBasket(tovar.id))}
+							>
+								<AiFillDelete />
+							</BasketItamDelete>
+						</BasketItam>
+					))}
+				</BasketList>
+				<BasketSum>
+					<samp>Сума: </samp>
+					<samp>
+						{tovars.reduce((a, b) => a + b.cost * b.baskeQuantity, 0)} грн
+					</samp>
+				</BasketSum>
+				<Buy to="/checkout">Оформити</Buy>
+			</BasketBox>
+		</BasketSection>,
+		portal
+	);
 };
 
 export default Basket;
