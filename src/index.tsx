4ca13a61e777f2cb9474ffef5b03bcbeecd1a372
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-28 12:46:38 +0300

    add loader

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { store, persistor } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import Loader from './reuseСomponents/loader/loader';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
@@ -14,7 +15,7 @@ root.render(
 	<React.StrictMode>
 		<BrowserRouter basename="/sunrise-hookah">
 			<Provider store={store}>
-				<PersistGate loading={<p>llll</p>} persistor={persistor}>
+				<PersistGate loading={<Loader opacity={1} />} persistor={persistor}>
 					<App />
 					<GlobalStyle />
 				</PersistGate>
