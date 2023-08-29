7e10371a93f38b3a786409b8aad989cc585dc819
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-29 13:48:26 +0300

    Update tovars.tsx

--- src/components/tovars/tovars.tsx
+++ src/components/tovars/tovars.tsx
@@ -1,23 +1,65 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useAppSelector } from '../../hooks';
 import { selectTypes } from '../../redux/types/slice';
-import { Link, NavUl, UlItem } from './tovars.style';
-
+import { Button, Link, NavUl, UlItem, UpdateTypeBox } from './tovars.style';
+import Modal from '../modal/modal';
+import { AiOutlineForm, AiOutlinePlusCircle } from 'react-icons/ai';
+import UpdateType from '../updateType/updateType';
+import { ModalBox } from '../tovarsDataTable/tovarsDataTable.style';
 
 type AdminPanelProps = {};
 
 const Tovars: FC<AdminPanelProps> = props => {
-    const Types = useAppSelector(selectTypes);
+	const [addType, setAddType] = useState<boolean>(false);
+	const [updateType, setUpdateType] = useState<boolean>(false);
+	const Types = useAppSelector(selectTypes);
 
-    return (
-        <div>
-            <NavUl>
-                {Types.map(type => <UlItem key={type.id}><Link to={type.en}>{type.ukr}</Link> </UlItem>)}
-            </NavUl>
-            <Outlet />
-        </div>
-    );
+	return (
+		<div>
+			<NavUl>
+				<UlItem key={'all'}>
+					<Link to={'all'}>Усі</Link>{' '}
+				</UlItem>
+				{Types.map(type => (
+					<UlItem key={type.id}>
+						<Link to={type.en}>{type.ukr}</Link>{' '}
+					</UlItem>
+				))}
+				<UlItem key={'all'}>
+					<Button onClick={() => setAddType(true)}>
+						<AiOutlinePlusCircle />
+					</Button>{' '}
+				</UlItem>
+				<UlItem key={'all'}>
+					<Button onClick={() => setUpdateType(true)}>
+						<AiOutlineForm />
+					</Button>{' '}
+				</UlItem>
+			</NavUl>
+			<Outlet />
+			{addType && (
+				<Modal openBasket={() => setAddType(false)}>
+					<ModalBox>
+						<UpdateType close={() => setAddType(false)} />
+					</ModalBox>
+				</Modal>
+			)}
+			{updateType && (
+				<Modal openBasket={() => setUpdateType(false)}>
+					<ModalBox>
+						<UpdateTypeBox>
+							{Types.map(type => (
+								<li>
+									<UpdateType type={type} />
+								</li>
+							))}
+						</UpdateTypeBox>
+					</ModalBox>
+				</Modal>
+			)}
+		</div>
+	);
 };
 
 export default Tovars;
