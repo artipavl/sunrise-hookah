353b6b3ba0112c1af6fc01dda15758e1fe4da756
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-29 13:47:32 +0300

    Update type redux

--- src/redux/types/slice.ts
+++ src/redux/types/slice.ts
@@ -1,44 +1,63 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { RootState } from '../store';
-import { fetchTovarsTypes } from './typesOperations';
+import {
+	addType,
+	fetchTovarsTypes,
+	putchTypeById,
+	removeTypeById,
+} from './typesOperations';
 
 export type Type = {
-  ukr: string;
-  en: string;
-  id: string;
+	ukr: string;
+	en: string;
+	id: string;
 };
 
 interface TovarsSliceType {
-  types: Type[];
-  typesIsLoading: boolean;
+	types: Type[];
+	typesIsLoading: boolean;
 }
 
 const initialState: TovarsSliceType = {
-  types: [],
-  typesIsLoading: false,
+	types: [],
+	typesIsLoading: false,
 };
 
 const typesSlice = createSlice({
-  name: 'types',
-  initialState,
-  reducers: {},
-  extraReducers: builder => {
-    builder
-      .addCase(fetchTovarsTypes.pending, state => {
-        state.typesIsLoading = false;
-      })
-      .addCase(fetchTovarsTypes.fulfilled, (state, action) => {
-        state.types = action.payload;
-        state.typesIsLoading = true;
-      })
-      .addCase(fetchTovarsTypes.rejected, (state ) => {
-        state.typesIsLoading = false;
-      });
-  },
+	name: 'types',
+	initialState,
+	reducers: {},
+	extraReducers: builder => {
+		builder
+			.addCase(fetchTovarsTypes.pending, state => {
+				state.typesIsLoading = false;
+			})
+			.addCase(fetchTovarsTypes.fulfilled, (state, action) => {
+				state.types = action.payload;
+				state.typesIsLoading = true;
+			})
+			.addCase(fetchTovarsTypes.rejected, state => {
+				state.typesIsLoading = false;
+			})
+			.addCase(addType.fulfilled, (state, action) => {
+				state.types.push(action.payload);
+			})
+			.addCase(removeTypeById.fulfilled, (state, action) => {
+				state.types = state.types.filter(type => type.id !== action.payload);
+			})
+			.addCase(putchTypeById.fulfilled, (state, action) => {
+				state.types = state.types.map(type => {
+					if (type.id === action.payload.id) {
+						return action.payload;
+					}
+					return type;
+				});
+			});
+	},
 });
 
 export const selectTypes = (state: RootState) => state.types.types;
 export const selectTypesIsLoading = (state: RootState) =>
-  state.types.typesIsLoading;
+	state.types.typesIsLoading;
 
 export default typesSlice.reducer;
--- src/redux/types/typesOperations.ts
+++ src/redux/types/typesOperations.ts
@@ -5,18 +5,78 @@ import { Type } from './slice';
 axios.defaults.baseURL = 'https://sunrise-hookah-api.onrender.com/';
 
 export const fetchTovarsTypes = createAsyncThunk(
-  'users/fetchTovarsTypes',
-  async (_, thunkAPI) => {
-    try {
-      const date = await axios.get(`type`);
+	'type/fetchTovarsTypes',
+	async (_, thunkAPI) => {
+		try {
+			const date = await axios.get(`type`);
 
-      if (date.status !== 200) {
-        return thunkAPI.rejectWithValue(date.data);
-      }
+			if (date.status !== 200) {
+				return thunkAPI.rejectWithValue(date.data);
+			}
 
-      return date.data as Type[];
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error);
-    }
-  }
+			return date.data as Type[];
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
+);
+
+export const addType = createAsyncThunk(
+	'type/addType',
+	async (type: { ukr: string; en: string }, thunkAPI) => {
+		try {
+			const date = await axios.post(`type`, { ...type });
+
+			if (date.status !== 200) {
+				return thunkAPI.rejectWithValue(date.data);
+			}
+
+			return date.data as Type;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
+);
+
+interface putchTypeByIdAction {
+	ukr: string;
+	en: string;
+	id: string;
+}
+
+export const putchTypeById = createAsyncThunk(
+	'type/putType',
+	async (action: putchTypeByIdAction, thunkAPI) => {
+		try {
+			const date = await axios.put(`type/${action.id}`, {
+				ukr: action.ukr,
+				en: action.en,
+			});
+			if (date.status !== 200) {
+				return thunkAPI.rejectWithValue(date.data);
+			}
+
+			// return date.data.tovars as Tovar[];
+			return date.data as Type;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
+);
+
+export const removeTypeById = createAsyncThunk(
+	'users/removeType',
+	async (id: string, thunkAPI) => {
+		try {
+			const date = await axios.get(`type/${id}`);
+			if (date.status !== 200) {
+				return thunkAPI.rejectWithValue(date.data);
+			}
+
+			// return date.data.tovars as Tovar[];
+			return id;
+		} catch (error) {
+			return thunkAPI.rejectWithValue(error);
+		}
+	}
 );
