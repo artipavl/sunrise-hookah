032793a7435ecdc5321bc8d268893594268e320e
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-08-29 13:48:24 +0300

    Update tovars.style.ts

--- src/components/tovars/tovars.style.ts
+++ src/components/tovars/tovars.style.ts
@@ -1,50 +1,80 @@
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const NavUl = styled.ul`
-    display: flex;
-    gap: 31px;
-    width: fit-content;
+	display: flex;
+	gap: 31px;
+	width: fit-content;
+	margin-bottom: 30px;
 
-    padding-inline: 15px;
+	padding-inline: 15px;
 
-    border-radius: 8px;
-    background: #FFF;
-    box-shadow: 0px 10px 30px 0px rgba(17, 38, 146, 0.05);
-
+	border-radius: 8px;
+	background: #fff;
+	box-shadow: 0px 10px 30px 0px rgba(17, 38, 146, 0.05);
 `;
 
 export const UlItem = styled.li`
-  display: flex;
-  align-items: center;
-  position: relative;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: relative;
 
-  :not(:last-child):after { 
-    content: ' ';
-    display: inline-block;
-    position: absolute;
-    right: -15px;
-    top: 15%;
-    width: 1px;
-    height: 70%;
-    background-color: #e9ecef;
-  }
-
+	:not(:last-child):after {
+		content: ' ';
+		display: inline-block;
+		position: absolute;
+		right: -15px;
+		top: 15%;
+		width: 1px;
+		height: 70%;
+		background-color: #e9ecef;
+	}
 `;
 
 export const Link = styled(NavLink)`
-  padding: 21px 25px;
-  text-transform: uppercase;
+	padding: 21px 25px;
+	text-transform: uppercase;
 
-  font-family: Inter;
-  font-size: 20px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 130%;
+	font-family: Inter;
+	font-size: 20px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 130%;
 
-  color: #232D42;
+	color: #232d42;
 
-  :hover, :focus, &.active {
-    color: red;
-  }
+	:hover,
+	:focus,
+	&.active {
+		color: red;
+	}
+`;
+
+export const Button = styled.button`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	padding: 21px 25px;
+	text-transform: uppercase;
+
+	font-family: Inter;
+	font-size: 20px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 130%;
+
+	color: #232d42;
+
+	:hover,
+	:focus,
+	&.active {
+		color: red;
+	}
+`;
+
+export const UpdateTypeBox = styled.ul`
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
 `;
