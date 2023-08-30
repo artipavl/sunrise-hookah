6b1aeaa94a0c0234916460ac2918c84a6b1bd4dd
Kindual <110603179+Kindual@users.noreply.github.com>
2023-08-30 17:56:22 +0300

    creating gallery

--- src/components/newTovar/newTovar.tsx
+++ src/components/newTovar/newTovar.tsx
@@ -1,6 +1,6 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import * as Yup from 'yup';
-import { withFormik, FormikProps, Form  } from 'formik';
+import { withFormik, FormikProps, Form } from 'formik';
 import {
 	BoxBtn,
 	BoxFields,
@@ -71,6 +71,73 @@ interface OtherProps {
 const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
 	const { touched, errors, isSubmitting, message } = props;
 	const types = useAppSelector(selectTypes);
+	const [fotosArray, setFotosArray] = useState<string[]>([]);
+
+	useEffect(() => {
+		async function createFotosArr(fotos: FileList): Promise<string[]> {
+			const fotosArr: string[] = [];
+			if (FileReader && fotos && fotos.length) {
+				for (let i = 0; i < fotos.length; i++) {
+					const fr = new FileReader();
+
+					fotosArr.push(await readFotos(fr, fotos[i]));
+				}
+			}
+
+			return fotosArr;
+		}
+
+		async function readFotos(fr: FileReader, file: File): Promise<string> {
+			return new Promise((resolve, reject) => {
+				fr.onload = () => {
+					resolve(fr.result as string);
+				};
+
+				fr.onabort = () => {
+					reject('Reading aborted');
+				};
+
+				fr.onerror = () => {
+					reject('Reading error');
+				};
+
+				fr.onloadend = () => {
+					console.log('Reading finished');
+				};
+
+				fr.readAsDataURL(file);
+			});
+		}
+
+		if (props.values.fotos) {
+			createFotosArr(props.values.fotos).then(value => {
+				console.log(value);
+				setFotosArray(value);
+			});
+		}
+	}, [props.values.fotos]);
+
+	// function createFotosArr(fotos: FileList) {
+	// 	const fotosArr: string[] = [];
+	// 	if (FileReader && fotos && fotos.length) {
+	// 		for (var i = 0; i < fotos.length; i++) {
+	// 			var fr = new FileReader();
+
+	// 			fr.onload = function () {
+	// 				const result = fr.result;
+	// 				fotosArr.push(result as string);
+	// 			};
+
+	// 			fr.readAsDataURL(fotos[i]);
+	// 			const file = fotos.item(i);
+	// 			file !== null && fotosArr.push(file ? file.name : '');
+	// 			console.log(fotos.item(i));
+	// 		}
+	// 	}
+
+	// 	console.log(fotos);
+	// 	return fotosArr;
+	// }
 
 	return (
 		<Form>
@@ -202,15 +269,25 @@ const InnerForm = (props: OtherProps & FormikProps<FormValues>) => {
 						// value={props.values.fotos}
 						onChange={event => {
 							props.setFieldValue('fotos', event.currentTarget.files);
-							console.log(props);
-							console.log(props.values);
 						}}
 					/>
 
-					<div> </div>
+					{/* <div>{props.values.fotos !== null && props.values.fotos[0].name }</div> */}
 
 					{touched.fotos && errors.fotos && <ErrorBox>{errors.fotos}</ErrorBox>}
 				</FormLabelFile>
+				<div>
+					{fotosArray.length && (
+						<ul>
+							{fotosArray.map((foto, index) => (
+								<li key={index}>
+									{' '}
+									<img src={foto} alt="" />
+								</li>
+							))}
+						</ul>
+					)}
+				</div>
 			</BoxFields>
 
 			<BoxBtn>
