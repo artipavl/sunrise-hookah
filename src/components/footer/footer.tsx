eadef88cd88c7eb9c845d474aeb8f29b78685449
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-09-05 18:20:17 +0300

    Update footer.tsx

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react';
-import { Footerbox } from './footet.style';
+import { AdminLink, Footerbox } from './footet.style';
 
 type FooterProps = {};
 
 const Footer: FC<FooterProps> = props => {
-  return <Footerbox>© 2023 SUNRISE HOOKAH</Footerbox>;
+	return (
+		<Footerbox>
+			© 2023 SUNRISE HOOKAH{' '}
+			<AdminLink to="/admin">Вхід для адміністратора</AdminLink>
+		</Footerbox>
+	);
 };
 
 export default Footer;
