d49002afdb7c20aedb75158e115ca6820d3679bf
Artem Paclichenko <40296816+artipavl@users.noreply.github.com>
2023-09-05 17:05:10 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
 	<React.StrictMode>
 		{/* <HashRouter> */}
-		<BrowserRouter basename="/sunrise-hookah">
+		<BrowserRouter>
 			<Provider store={store}>
 				<PersistGate loading={<Loader opacity={1} />} persistor={persistor}>
 					<App />
