d79214d22519a192730759eaf9e4e9882c2b9a1b
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-09-10 21:38:25 +0300

    add language

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -14,58 +14,58 @@ import {
   AboutTitle,
   Aboutlist,
 } from './about.style';
+import { useAppSelector } from '../../hooks';
+import { selectLanguage } from '../../redux/language/languageSelectors';
 
 type AboutProps = {};
 
 const About: FC<AboutProps> = props => {
+  const language = useAppSelector(selectLanguage);
   return (
     <Section id="about" bgc="#000" h={true}>
       <Container flex={'center'}>
-        <AboutTitle>SUNRISE HOOKAH - це</AboutTitle>
+        <AboutTitle>{language==="uk" ? "SUNRISE HOOKAH" : "SUNRISE HOOKAH is"}</AboutTitle>
         <Aboutlist>
           <AboutItem>
             <QUALITY color="#fff" width={'30%'} />
-            <AboutItemTitle>ЯКІСНІ МАТЕРІАЛИ</AboutItemTitle>
+            <AboutItemTitle>{language==="uk" ? "ЯКІСНІ МАТЕРІАЛИ" : "QUALITY MATERIALS"} </AboutItemTitle>
             <AboutItemText>
-              Виготовлений з нержавіючої сталі марки AISI 304 і якісної
-              поліацеталі на нашому виробництві.
+            {language==="uk" ? "Виготовлений з нержавіючої сталі марки AISI 304 і якісної поліацеталі на нашому виробництві." : "Made of AISI 304 stainless steel and high quality polyacetals in our production."}
             </AboutItemText>
           </AboutItem>
           <AboutItem>
             <Stars color="#fff" width={'30%'} />
-            <AboutItemTitle>КОМФОРТ ПАЛІННЯ</AboutItemTitle>
+            <AboutItemTitle>{language==="uk" ? "КОМФОРТ ПАЛІННЯ" : "COMFORT OF SMOKING"} </AboutItemTitle>
             <AboutItemText>
-              Легка, приємна тяга. Унікальна продувка за 1-2 рази.
+            {language==="uk" ? "Легка, приємна тяга. Унікальна продувка за 1-2 рази." : "Light, pleasant pull. Unique purging in 1-2 times."}
+              
             </AboutItemText>
           </AboutItem>
           <AboutItem>
             <Gear color="#fff" width={'30%'} />
-            <AboutItemTitle>ЗРУЧНІСТЬ ЗБІРКИ</AboutItemTitle>
+            <AboutItemTitle>{language==="uk" ? "ЗРУЧНІСТЬ ЗБІРКИ" : "CONVENIENCE OF ASSEMBLY"} </AboutItemTitle>
             <AboutItemText>
-              Відсутня різьба в полаіцеталі. Порт на щільній притирці. Шахта
-              розбірна.
+            {language==="uk" ? "Відсутня різьба в полаіцеталі. Порт на щільній притирці. Шахта розбірна." : "There is no thread in the polyacetal. Port on a tight lapping. The mine is collapsible."}
             </AboutItemText>
           </AboutItem>
           <AboutItem>
             <Moon color="#fff" width={'30%'} />
-            <AboutItemTitle>ДИЗАЙН</AboutItemTitle>
+            <AboutItemTitle>{language==="uk" ? "ДИЗАЙН" : "DESIGN"} </AboutItemTitle>
             <AboutItemText>
-              Великий вибір моделей, декоративних накладок та колб. Блюдце
-              Орбіта унікальної форми.
+            {language==="uk" ? "Великий вибір моделей, декоративних накладок та колб. Блюдце Орбіта унікальної форми." : "A large selection of models, decorative overlays and flasks. Orbit saucer of a unique shape."}
             </AboutItemText>
           </AboutItem>
           <AboutItem>
             <Biceps color="#fff" width={'30%'} />
-            <AboutItemTitle>НАДІЙНІСТЬ</AboutItemTitle>
+            <AboutItemTitle>{language==="uk" ? "НАДІЙНІСТЬ" : "RELIABILITY"} </AboutItemTitle>
             <AboutItemText>
-              TIAGA HOOKAH - український виробник кальянів, на ринку з 2018
-              року.
+            {language==="uk" ? "SUNRISE HOOKAH - український виробник кальянів, на ринку з 2018 року." : "SUNRISE HOOKAH is a Ukrainian manufacturer of hookahs, on the market since 2018."} 
             </AboutItemText>
           </AboutItem>
           <AboutItem>
             <Coins color="#fff" width={'30%'} />
-            <AboutItemTitle>ДОСТУНА ЦІНА</AboutItemTitle>
-            <AboutItemText>Топова якість за вигідну ціну.</AboutItemText>
+            <AboutItemTitle>{language==="uk" ? "ДОСТУПНА ЦІНА" : "AFFORDABLE PRICE"}</AboutItemTitle>
+            <AboutItemText>{language==="uk" ? "Топова якість за вигідну ціну." : "Top quality for a good price."}</AboutItemText>
           </AboutItem>
         </Aboutlist>
       </Container>
