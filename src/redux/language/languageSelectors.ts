1f29c6bdedccf01b47168c586659199f20bdc9cd
Artem Pavlichenko <40296816+artipavl@users.noreply.github.com>
2023-09-10 20:57:54 +0300

    Update languageSelectors.ts

--- src/redux/language/languageSelectors.ts
+++ src/redux/language/languageSelectors.ts
@@ -1,4 +1,4 @@
 import { RootState } from '../store';
 
-export const selectBasket = (state: RootState) => state.basket.tovars;
+export const selectBasket = (state: RootState) => state.language.language;
 
